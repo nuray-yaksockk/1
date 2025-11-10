aba080f22e90b77269416b88d6f294259bfcd042
astan <nuraynrh@gmail.com>
2025-11-10 22:40:00 +0500

    Initialize Go module and add number checker

---
+++ main1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	number := -7
+
+	fmt.Printf("Проверяем число: %d\n", number)
+
+	// Проверяем условия
+	if number == 0 {
+		fmt.Println("Число является нулем")
+	} else if number > 0 {
+		if number%2 == 0 {
+			fmt.Printf("Число %d - положительное четное\n", number)
+		} else {
+			fmt.Printf("Число %d - положительное нечетное\n", number)
+		}
+	} else {
+		fmt.Printf("Число %d - отрицательное\n", number)
+	}
+}
